eb8dcf4329796c8b589c2eaf27f2d22b01a567e6
Olamide Emmanuel Willoughby <willoughbyolamideemmanuel@gmail.com>
2026-05-20 22:36:50 +0100

    Add trailers.js with video metadata

---
+++ trailers.js
@@ -0,0 +1,12 @@
+const trailers = [
+    {
+        id: "YOUTUBE_VIDEO_ID_HERE",
+        title: "Your Video Title Here",
+        description: "Short description of the video"
+    },
+    {
+        id: "ANOTHER_VIDEO_ID",
+        title: "Another Video Title",
+        description: "Another short description"
+    }
+];
