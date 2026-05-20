3ea7770a720509152465e1fdd4f41c75679a00dc
Olamide Emmanuel Willoughby <willoughbyolamideemmanuel@gmail.com>
2026-05-20 22:44:24 +0100

    Update video IDs and clear titles in trailers
    Updated video IDs in trailers array and cleared titles.

--- trailers.js
+++ trailers.js
@@ -1,12 +1,14 @@
 const trailers = [
     {
-        id: "YOUTUBE_VIDEO_ID_HERE",
-        title: "Your Video Title Here",
-        description: "Short description of the video"
+        id: "8G2GCljkJfA",
+        title: ""
     },
     {
-        id: "ANOTHER_VIDEO_ID",
-        title: "Another Video Title",
-        description: "Another short description"
+        id: "-W7kh0XYwgg",
+        title: ""
+    },
+    {
+        id: "b-eW0sUprrI",
+        title: ""
     }
 ];
